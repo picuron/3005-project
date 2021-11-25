a9ebb7bbe100cfd261e418b7571aee2dc1bc4070
Eric <eric.herscovich@shopify.com>
2021-11-25 13:43:22 -0500

    Db

--- lib/Database/InitDB.rb
+++ lib/Database/InitDB.rb
@@ -1,14 +1,41 @@
+require 'pg'
+
 module Database
   class InitDB
     
     def initalize
-      message
+      create_user
     end
 
     private
 
-    def message
-      puts "Inside InitDB"
+    def create_user
+      # system("brew services start postgresql")
+
+      # system("brew services stop postgresql")
+
+      begin
+
+        con = PG.connect :dbname => 'testdb', :user => 'postgres', 
+            :password => 'Insert Password Here'
+    
+        user = con.user
+        db_name = con.db
+        pswd = con.pass
+        
+        puts "User: #{user}"
+        puts "Database name: #{db_name}"
+        # puts "Password: #{pswd}" 
+        
+        rescue PG::Error => e
+        
+            puts e.message 
+            
+        ensure
+    
+        con.close if con
+        
+    end
     end
   end
 end
